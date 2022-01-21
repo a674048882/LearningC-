0ddda4122d10d3a68fa304737e7502ff9017cade
Huzejun <fuuuulrr@gmail.com>
2022-01-21 20:34:32 +0800

    practices from chapter 1

---
+++ C++Primer_part1/1.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "please enter 2 numbers:" << endl;
+    int v1, v2;
+    cin >> v1 >> v2;
+    cout << v1 << "and" << v2 << "equals" << v1*v2 << endl;
+
+    return 0;
+
+}
