b0048f680340b5b31bcc8c6b5a06ddfba2eb90c6
Huzejun <fuuuulrr@gmail.com>
2022-02-11 22:07:09 +0800

    ex20

---
+++ C++Primer_start/ex20.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Sales_item.h"
+using namespace std;
+
+int main ()
+{
+    Sales_item book;
+    cout << "请输入销售记录：" << endl;
+    while (cin >> book)
+    {
+        cout << "ISBN、售出本数、销售额和平均售价为 " << book << endl;
+    }
+    return 0;
+}
